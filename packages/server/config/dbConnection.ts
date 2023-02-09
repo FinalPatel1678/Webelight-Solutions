b021649ac8f6ef2a45c8a011d872e3a3e7c09d1c
Final Patel <final16700@gmail.com>
2023-02-09 22:22:58 +0530

    chore: added code to add dummy data in DB

--- packages/server/config/dbConnection.ts
+++ packages/server/config/dbConnection.ts
@@ -13,7 +13,27 @@ const connectDatabase = () => {
     .then(async (db) => {
       console.log("Mongoose Connected");
 
+      // Uncomment code once to add dummy data in database
+
       // const data: IProductRequest[] = [];
+      // for (let t = 0; t < 15; t++) {
+      //   data.push({
+      //     category: "categoryA",
+      //     favorite: false,
+      //     price: Math.random() * 100,
+      //     productImage: "/logo192.png",
+      //     productName: "Product" + " " + t + 1,
+      //   });
+      // }
+      // for (let t = 1; t < 30; t++) {
+      //   data.push({
+      //     category: "categoryB",
+      //     favorite: true,
+      //     price: Math.random() * 100,
+      //     productImage: "/logo192.png",
+      //     productName: "Product" + " " + t + 1,
+      //   });
+      // }
       // for (let t = 30; t < 45; t++) {
       //   data.push({
       //     category: "categoryC",
